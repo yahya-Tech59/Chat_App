d96b33c0b84f3a69ef78dfe4f140c091001d38d5
yahya-cade <yahyekade790@gmail.com>
2023-09-03 12:37:36 +0300

    messageForm updated

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { sendMessage, isTyping } from "react-chat-engine";
+import { AiFillPicture } from "react-icons/ai";
+import { BsSendFill } from "react-icons/bs";
 
-export const MessageForm = () => {
-  return <div>MessageForm</div>;
+export const MessageForm = (props) => {
+  const [value, setValue] = useState("");
+  const { chatId, creds } = props;
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    isTyping(props, chatId);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const text = value.trim();
+
+    if (text.length > 0) {
+      sendMessage(creds, chatId, { text });
+    }
+    setValue("");
+  };
+
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: "" });
+  };
+
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <AiFillPicture className="picture-icon" />
+        </span>
+      </label>
+
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: "none" }}
+        onChange={handleUpload.bind(this)}
+      />
+
+      <button type="submit" className="send-button">
+        <BsSendFill className="send-icon" />
+      </button>
+    </form>
+  );
 };
